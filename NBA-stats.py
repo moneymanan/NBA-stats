79af945a35204a8e8745492a2cfe4462aca684a7
moneymanan <wrt2manan@gmail.com>
2026-03-18 20:43:30 -0400

    Update NBA-stats.py

--- NBA-stats.py
+++ NBA-stats.py
@@ -2,65 +2,54 @@
 import pandas as pd
 import difflib
 import sys
+import streamlit as st
+
 
 s = pd.read_csv(r"Moneymanan\My Stuff\Python\NBA Stats\PlayerStatistics.csv", usecols=range(32), dtype={8: "string", 9: "string"})
 g = pd.read_csv(r"Moneymanan\My Stuff\Python\NBA Stats\Games.csv", usecols=range(12), dtype={8: "string", 10: "string"})
 
-#print(s.iloc[0, 0:2]) # Devin Booker
-#print(s.at[0,'firstName']) # Devin
+st.title(":sunglasses: NBA Stats :sunglasses:", text_alignment = "center")
+st.header("Trying to fulfill my NBA dreams", text_alignment = "center")
 
 valid_stats = list(s.columns)
 s["player"] = s["firstName"] + " " + s["lastName"]
 players = s["player"].unique()
-name = input("Name: ")
-if ' ' not in name:
-    name = input('Enter first and last name: ')
+name = st.text_input("Name: ")
+while ' ' not in name:
+    name = st.text_input('Enter first and last name: ')
 while name not in players:
     suggestion = difflib.get_close_matches(name, players, n=1, cutoff=0.6)
     if suggestion:
-        opt = input(f"Do you mean '{suggestion[0]}'?\nY/N: ")
-        if opt.lower() =='y':
+        opt = st.write(f"Do you mean '{suggestion[0]}'?")
+        if st.button("Yes"): #opt.lower() =='y':
             name = suggestion[0]
-        else:
-            name = input("Name: ")
+        elif st.button("No"):
+            name = st.text_input("Name: ")
     else:
         sys.exit(':( NAME ERROR :(')
-#fname, lname = name.split(maxsplit=1)
 
-stat = input("What stat do you want? (points, rebounds, assists, blocks, steals): ").lower()
+stat = st.text_input("What stat do you want? (points, rebounds, assists, blocks, steals): ").lower()
 while stat.lower() not in [col.lower() for col in valid_stats]:
     suggestion = difflib.get_close_matches(stat, valid_stats, n=1)
     if suggestion:
-        opt = input(f"Do you mean '{suggestion[0]}'?\nY/N: ")
-        if opt.lower() == 'y':
+        opt = st.write(f"Do you mean '{suggestion[0]}'?")
+        if st.button("Yes"): #opt.lower() == 'y':
             stat = suggestion[0]
-        else:
-            stat = input("Enter stat: ")
+        elif st.button("No"):
+            stat = st.text_input("Enter stat: ")
     else:
-        print(':( ERROR :(')
         sys.exit('LAPTOP DESTRUCTION EMINENT\n'*5) 
 
-#stat1 = stat
 info = s.loc[
     s["player"] == name,
     stat
 ].iloc[0]
-#info = s.loc[
-#    (s["firstName"].str.lower() == fname.lower()) & (s["lastName"].str.lower() == lname.lower()),
-#    stat1                                    
-#].iloc[0]
+
 
 if name.lower() == 'lebron james':
-    name = 'The GOAT'
-    print(name + ' had ' + str(int(info)) + ' ' + stat + ' :)')
+    name = 'THE GOAT'
+    st.write(name + ' had ' + str(int(info)) + ' ' + stat + ' :sunglasses:')
 else:
-    print(s.loc[s['player'] == name, 'playerteamCity'].iloc[0] + ' ' + s.loc[s['player'] == name, 'playerteamName'].iloc[0] + 
-      ' player ' + name + ' had ' + str(int(info)) + ' ' + stat + ' :)')
-#if ' ' in lname: 
-#    lname, third = lname.split() 
-#   if 'ii' in third.lower():
-#       print(fname.capitalize() + ' ' + lname.capitalize() + ' ' + third.upper() + " had " + str(int(info)) + ' ' + stat + ' :)')
-#    else:    
-#        print(fname.capitalize() + ' ' + lname.capitalize() + ' ' + third.capitalize() + " had " + str(int(info)) + ' ' + stat + ' :)')
-#else:
-#    print(fname.capitalize() + ' ' + lname.capitalize() + " had " + str(int(info)) + ' ' + stat + ' :)')
+    st.write(s.loc[s['player'] == name, 'playerteamCity'].iloc[0] + ' ' + s.loc[s['player'] == name, 'playerteamName'].iloc[0] + 
+      ' player ' + name + ' had ' + str(int(info)) + ' ' + stat + ' :sunglasses:')
+
